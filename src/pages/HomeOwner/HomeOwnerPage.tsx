f6ba7a111b966667b65c9ea2db3748773013b91b
LTDoanh <125759416+LTDoanh@users.noreply.github.com>
2026-04-22 22:18:48 +0700

    Update HomeOwnerPage minor changes

--- src/pages/HomeOwner/HomeOwnerPage.tsx
+++ src/pages/HomeOwner/HomeOwnerPage.tsx
@@ -492,7 +492,9 @@ const HomeOwnerPage: React.FC = () => {
                       </Text>
                       {building.address && (
                         <Text style={{ fontSize: 14, color: "#666", marginTop: 4 }}>
-                          <BuildingFillIcon size={14} color="black" style={{ display: "inline-block", verticalAlign: "middle", margin: "0 4px 2px 0" }} />
+                          <svg width="14" height="14" viewBox="0 0 24 24" fill="black" xmlns="http://www.w3.org/2000/svg" style={{ display: "inline-block", verticalAlign: "middle", margin: "0 4px 2px 0" }}>
+                            <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z" />
+                          </svg>
                           {building.address}
                         </Text>
                       )}
